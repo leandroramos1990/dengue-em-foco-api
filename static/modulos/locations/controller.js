f9a90e67241628d798d55e1525b062fa3675c0ce
Leandro Ramos <leandroramos.crz@gmail.com>
2016-04-26 21:44:08 -0300

    Remoção de filtro quando a opção é Todos

--- static/modulos/locations/controller.js
+++ static/modulos/locations/controller.js
@@ -4,7 +4,7 @@ var listar = function(req, res){
 	Location.find({}, function (err, locations){
         
         if(!err){
-            var todosFocos = {_id:0, title: "Todos os locais", loc:[0, 0]}
+            var todosFocos = {_id:0, title: "Todos", loc:[0, 0]}
             locations.unshift(todosFocos);
 		    res.status(200).json({locations: locations});     
         }
@@ -15,19 +15,23 @@ var listar = function(req, res){
 var localizarProximo = function(req, res) {
 	var lat = req.params.lat;
 	var lng = req.params.lng;
-
-	var query = {
-			  loc:{
-				    $near: {
-				      $geometry:{
-				        type:"Point",
-				        coordinates:[lat, lng] //Longitude, Latitude
-				      },
-				      $maxDistance: 10000, //Metros (10 km)
-				      $minDistance: 0  //Metros
-				    }
-				  }
-				};
+    var query = {};
+    
+    if(lat != 0 && lng != 0) {
+      var query = {
+      loc:{
+            $near: {
+              $geometry:{
+                type:"Point",
+                coordinates:[lat, lng] //Longitude, Latitude
+              },
+              $maxDistance: 10000, //Metros (10 km)
+              $minDistance: 0  //Metros
+            }
+          }
+        };
+    } 
+      
 	Location.find(query, function (err, locations){
 		if(!err){
 			res.status(200).json(locations);
